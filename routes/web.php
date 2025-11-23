1d30455d617931ed9e63e17b0603533b5496aacf
Abdur Rachman <marqq12n@gmail.com>
2025-11-23 23:01:10 +0700

    ubah landinh
    Added a route for the home page view for guests.

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\ProdiController;
 
 // Auth (guest only)
 Route::middleware('guest')->group(function () {
+    Route::get('/', fn() => view('pages.pengumuman'));
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.perform');
 });
@@ -32,7 +33,6 @@ Route::middleware(['auth'])->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::get('/', fn() => view('pages.pengumuman'));
     Route::get('/pengumuman', fn() => view('pages.pengumuman'));
 
 
